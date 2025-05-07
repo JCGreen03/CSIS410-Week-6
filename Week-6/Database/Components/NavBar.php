cb02ce983b0039f6ce8875d874a02b1d774142a0
JCGreen03 <160683432+JCGreen03@users.noreply.github.com>
2025-05-07 08:13:20 -0400

    Delete Week-6 directory

--- Week-6/Database/Components/NavBar.php
+++
@@ -1,8 +0,0 @@
-<?php
-    echo '<ul class="navbar">
-    <li><a class="link" href="/HomeMenu.php">Back to Menu</a></li>
-    <li><a class="link" href="CommentsHome.php">Home</a></li>
-     </ul>';
-    ?>
-
-            
